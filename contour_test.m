4a5f4624f72c5b3d63f502268c578c49ee025794
lvyuan <1322296910@qq.com>
2019-04-30 21:16:44 +0800

    add reheat cycle

--- contour_test.m
+++ contour_test.m
@@ -7,3 +7,13 @@ Z = X.*exp(-X.^2-Y.^2);
 set(h,'ShowText','on','TextStep',get(h,'LevelStep')*1)
 
 colormap cool
+
+y=[1 2;3 4; 5 6];
+b=bar(y);
+ch = get(b,'children');
+set(ch{1},'facecolor',[0 0 0])
+set(ch{2},'facecolor',[1 1 1])
+legend([ch{1} ch{2}],'AA','BB');
+xlabel('XXXXX');
+ylabel('YYYYY');
+set(gca,'XTickLabel',{'30%','45%','60%'})
